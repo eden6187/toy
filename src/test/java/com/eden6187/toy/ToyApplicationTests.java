fcefcc5f4ec477b1ead165b78cb48087141200d4
김영현 <gim-yeonghyeon@gim-yeonghyeon-ui-MacBookPro.local>
2024-01-16 21:08:33 +0900

    first commit

---
+++ src/test/java/com/eden6187/toy/ToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eden6187.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
