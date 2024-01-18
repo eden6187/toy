a4930d2054b40475c432e62192edabfe404a7946
eden6187 <ngolo6187@gmail.com>
2024-01-18 22:08:59 +0900

    modify appspec.yml and scripts

--- src/main/java/com/eden6187/toy/ToyApplication.java
+++ src/main/java/com/eden6187/toy/ToyApplication.java
@@ -2,12 +2,16 @@ package com.eden6187.toy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ToyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ToyApplication.class, args);
+        SpringApplication app = new SpringApplication(ToyApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
     }
 
 }
