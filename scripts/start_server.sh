7626ee1f8d085a019375e4a6359c96a6cab48b98
eden6187 <ngolo6187@gmail.com>
2024-01-30 21:38:37 +0900

    modify buid_project script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 
 REPOSITORY='/home/ubuntu/web_app/app'
 cd ${REPOSITORY} || exit
-sudo nohup java -jar ./toy-0.0.1-SNAPSHOT.jar 1>/dev/null 2>&1
+sudo nohup java -jar ./toy-0.0.1-SNAPSHOT.jar 1>/dev/null 2>&1 &
